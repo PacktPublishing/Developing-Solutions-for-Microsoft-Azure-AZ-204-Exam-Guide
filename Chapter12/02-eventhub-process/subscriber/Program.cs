97ec3675e5d0096c4d789178072c54e401f4ae6b
Alex Ivanov <aivanofff@gmail.com>
2022-05-13 10:02:53 -0400

    chapter 12 inital

---
+++ Chapter12/02-eventhub-process/subscriber/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.EventHubs;
+using Microsoft.Azure.EventHubs.Processor;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace subscriber
+{ 
+    class Program
+    {
+        
+        private const string EventHubConnectionString = "<you event hub connection string from previous script run>";
+        private const string StorageConnectionString  = "<you storage account connection string from previous script run>";
+
+        public static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+        private static async Task MainAsync(string[] args)
+        {
+            // Creates an EventHubsConnectionStringBuilder object from a the connection string, and sets the EntityPath.
+            // Typically the connection string should have the Entity Path in it, but for the sake of this simple scenario
+            // we are using the connection string from the namespace.
+            var connectionStringBuilder = new EventHubsConnectionStringBuilder(EventHubConnectionString);
+
+            var eventProcessorHost = new EventProcessorHost(
+            EventHubConnectionString.Split("EntityPath=")[1],
+            PartitionReceiver.DefaultConsumerGroupName,
+            EventHubConnectionString,
+            StorageConnectionString,"capdate");
+
+            await eventProcessorHost.RegisterEventProcessorAsync<SimpleEventProcessor>();
+
+            Console.WriteLine("Receiving. Press enter key to stop worker.");
+            Console.ReadLine();
+
+            // Disposes of the Event Processor Host
+            await eventProcessorHost.UnregisterEventProcessorAsync();            
+
+        }
+    }
+
+    public class SimpleEventProcessor : IEventProcessor
+    {
+        public Task CloseAsync(PartitionContext context, CloseReason reason)
+        {
+            Console.WriteLine($"Processor Shutting Down. Partition '{context.PartitionId}', Reason: '{reason}'.");
+            return Task.CompletedTask;
+        }
+
+        public Task OpenAsync(PartitionContext context)
+        {
+            Console.WriteLine($"SimpleEventProcessor initialized. Partition: '{context.PartitionId}'");
+            return Task.CompletedTask;
+        }
+
+        public Task ProcessErrorAsync(PartitionContext context, Exception error)
+        {
+            Console.WriteLine($"Error on Partition: {context.PartitionId}, Error: {error.Message}");
+            return Task.CompletedTask;
+        }
+
+        public Task ProcessEventsAsync(PartitionContext context, IEnumerable<EventData> messages)
+        {
+            foreach (var eventData in messages)
+            {
+                var data = Encoding.UTF8.GetString(eventData.Body.Array, eventData.Body.Offset, eventData.Body.Count);
+                Console.WriteLine($"Message received. Partition: '{context.PartitionId}', Data: '{data}'");
+            }
+
+            return context.CheckpointAsync();
+
+        }
+    }
+}
---
+++ Chapter12/04-eventgrid-process/publisher/Program.cs
@@ -0,0 +1,37 @@
+﻿using Azure;
+using Azure.Messaging.EventGrid;
+using System;
+using System.Threading.Tasks;
+
+namespace publisher
+{
+    class Program
+    {
+        static string endpoint = "<your custom topic endpoint>";
+        static string key = "<your custom topic access key>";
+
+        static void Main(string[] args)
+        {
+            Send().Wait();
+
+        }
+
+        static async Task Send()
+        {
+                EventGridPublisherClient client = new EventGridPublisherClient(
+                new Uri(endpoint),
+                new AzureKeyCredential(key));
+
+                        // Add EventGridEvents to a list to publish to the topic
+                        EventGridEvent egEvent =
+                            new EventGridEvent(
+                                "ExampleEventSubject",
+                                "Example.EventType",
+                                "1.0",
+                                "This is the event data");
+
+                        // Send the event
+                        await client.SendEventAsync(egEvent);
+        }
+    }
+}
