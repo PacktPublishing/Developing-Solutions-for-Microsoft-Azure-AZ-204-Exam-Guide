45f7a60716628ecb766ac1d97ef2af7c0c1dec6f
Paul Ivey <paivey@microsoft.com>
2022-06-03 16:31:18 +0100

    Added interactive auth code sample

---
+++ Chapter07/01-interactive-auth/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Identity.Client;
+
+const string _clientId = "Put your app/client ID here";
+const string _tenantId = "Put your tenant ID here";
+
+var app = PublicClientApplicationBuilder
+    .Create(_clientId)
+    .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+    .WithRedirectUri("http://localhost")
+    .Build();
+
+string[] scopes = { "User.Read", "Calendars.Read" };
+
+AuthenticationResult result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+
+Console.WriteLine($"ID:\n{result.IdToken}");
+Console.WriteLine($"Access:\n{result.AccessToken}");
