c23c972f747ffdcc14cb7146c6420cb37a1695c5
Paul Ivey <paivey@microsoft.com>
2022-06-21 16:27:53 +0100

    Chapter08 files uploaded

---
+++ Chapter08/05-feature-flags/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.FeatureManagement.Mvc;
+
+namespace _05_feature_flags.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
---
+++ Chapter08/05-feature-flags/Program.cs
@@ -0,0 +1,54 @@
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+using Microsoft.FeatureManagement;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddUserSecrets<Program>();
+
+var connectionString = builder.Configuration.GetConnectionString("AppConfig");
+
+builder.Host.ConfigureAppConfiguration((hostingContext, builder) =>
+{
+    builder.AddAzureAppConfiguration(options =>
+    {
+        options.Connect(connectionString)
+               .UseFeatureFlags(option =>
+               {
+                option.Select("demofeature", hostingContext.HostingEnvironment.EnvironmentName);
+               })
+               .ConfigureRefresh(refreshOptions =>
+               {
+                   refreshOptions.Register("demofeature");
+               });
+    });
+});
+
+builder.Services.AddAzureAppConfiguration()
+                .AddFeatureManagement();
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+// Use the App Configuration middleware for dynamic refresh
+app.UseAzureAppConfiguration();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
